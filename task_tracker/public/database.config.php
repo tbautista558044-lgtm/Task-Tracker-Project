c027109f8e8fb01be49ff4fec481d277985fe707
Trisha Kate <t.bautista.558044@umindanao.edu.ph>
2026-05-25 07:57:56 +0800

    Update database.config.php

--- task_tracker/public/database.config.php
+++ task_tracker/public/database.config.php
@@ -4,7 +4,7 @@ define('DB_HOST', getenv('MYSQLHOST')     ?: getenv('DB_HOST') ?: 'localhost');
 define('DB_PORT', (int)(getenv('MYSQLPORT') ?: getenv('DB_PORT') ?: 3306));
 define('DB_USER', getenv('MYSQLUSER')     ?: getenv('DB_USER') ?: 'root');
 define('DB_PASS', getenv('MYSQLPASSWORD') ?: getenv('DB_PASS') ?: '');
-define('DB_NAME', getenv('MYSQLDATABASE') ?: getenv('DB_NAME') ?: 'task_tracker1');
+define('DB_NAME', getenv('MYSQLDATABASE') ?: getenv('DB_NAME') ?: 'task_tracker');
 
 $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
 
